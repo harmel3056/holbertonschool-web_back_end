4661c7fa6d3ec165280489470f49d02e302d7b0a
harmel3056 <melh1988@hotmail.com>
2025-11-18 16:14:12 +1100

    Type annotation on list of floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,8 +2,9 @@
 """
 Demonstrates type-annotation on a basic function
 """
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Returns the sum of all float values in the input list."""
     return sum(input_list)
