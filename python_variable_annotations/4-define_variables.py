0297fd670bb4cf029e8ffd5bc111ff3f15977f60
harmel3056 <melh1988@hotmail.com>
2025-11-18 15:45:12 +1100

    Annotation on variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -3,7 +3,7 @@
 Demonstrates type-annotation on variables
 """
 
-a: int = 1;
-pi: float = 3.14;
-i_understand_annotations: bool = True;
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
 school: str = "Holberton"
