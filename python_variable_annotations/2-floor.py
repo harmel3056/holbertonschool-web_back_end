0815606dff400b485cfec2632813a7b30acf3e3e
harmel3056 <melh1988@hotmail.com>
2025-11-18 14:59:36 +1100

    Rounds down to nearest int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Demonstrates type-annotation on a basic function
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """Returns the floor of n"""
+    return math.floor(n)
