8a41409222193032f36e4021ccf587f08ed42ef7
harmel3056 <melh1988@hotmail.com>
2025-11-18 14:26:39 +1100

    Annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Demonstrates type-annotation on a basic function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two floats"""
+    return a + b
