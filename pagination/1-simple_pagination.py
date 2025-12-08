8a207134aa2f01c27a0b57c7ca91fdc56c86c6a8
harmel3056 <melh1988@hotmail.com>
2025-12-08 19:03:45 +1100

    Pagination at work using a db, extracts the index items based on page number and number of objects per page

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Calculates the start and end indexes for a given page of data.
+    """
+    start = page_size * (page - 1)
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Extracts index items based on page number and number of items per page
+        """
+        assert (
+            isinstance(page, int)
+            and page > 0
+        ), "page should be greater than 0"
+        assert (
+            isinstance(page_size, int)
+            and page_size > 0
+        ), "page_size should be greater than 0"
+
+        start, end = index_range(page, page_size)
+        items_returned = self.dataset()[start:end]
+        return items_returned
