f3eb66707a85208fd93637ac9dd438400af820e0
harmel3056 <melh1988@hotmail.com>
2025-12-16 17:58:30 +1100

    Checker did not like my notes

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
-// Uses default values to keep a succinct function
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
