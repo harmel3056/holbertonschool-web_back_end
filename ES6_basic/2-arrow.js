2588e9967e821017ab5306631345b06ca9f8ce44
harmel3056 <melh1988@hotmail.com>
2025-12-16 17:50:41 +1100

    Arrow syntax with this

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+// ES6 arrow syntax
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
