23309570a2129b47bd97416cb51c1eeb4d512301
harmel3056 <melh1988@hotmail.com>
2025-12-16 17:56:52 +1100

    Uses default values to prevent function from becoming too verbose

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,4 @@
+// Uses default values to keep a succinct function
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
