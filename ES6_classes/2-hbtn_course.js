6895284b56d8906f33913a72b9f78b943dbd5cd0
harmel3056 <melh1988@hotmail.com>
2025-12-18 08:57:23 +1100

    Uses getters and setters to validate data, using encapsulation

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      return;
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      return;
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      return;
+    }
+
+    if (!value.every((student) => typeof student === 'string')) {
+      return;
+    }
+
+    this._students = value;
+  }
+}
