93aa731fb45ad77831a102b515fa909a91f96545
harmel3056 <melh1988@hotmail.com>
2025-12-18 10:31:09 +1100

    Makes a mock abstract class that verifies if any classes extending from it are using a certain method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,13 +5,13 @@ export default class Building {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          'Class extending Building must override evacuationWarningMessage'
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
   }
 
   get sqft() {
-    return this_sqft;
+    return this.sqft;
   }
 }
