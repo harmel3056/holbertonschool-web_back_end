4cec4b770a447018d0e1253dd9283d2149d6fdc1
harmel3056 <melh1988@hotmail.com>
2025-12-18 14:41:56 +1100

    Remove class extension to appease my checker overlords

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -16,11 +16,11 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export class StudentHolberton extends HolbertonClass {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-    super(holbertonClass.year, holbertonClass.location);
     this._firstName = firstName;
     this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
   get firstName() {
@@ -31,16 +31,12 @@ export class StudentHolberton extends HolbertonClass {
     return this._lastName;
   }
 
-  get year() {
-    return this._year;
-  }
-
-  get location() {
-    return this._location;
+  get holbertonClass() {
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._year} - ${this._location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} - ${this._holbertonClass._location}`;
   }
 }
 
