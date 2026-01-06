74df881627e4969079578c02e9e145049167cbf8
harmel3056 <melh1988@hotmail.com>
2026-01-06 17:52:13 +1100

    Uses the filter function to extract students based on a corresponding city entered

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,8 @@
+/* Takes a list of students(array) and a city(string)
+to facilitate the filter. The studentList database is filtered by
+(student) as each array item, and the final function is enacted to 
+match the location of the student to the city parameter entered */
+
 export default function getStudentsByLocation(studentList, city) {
   return studentList.filter((student) => student.location === city);
 }
