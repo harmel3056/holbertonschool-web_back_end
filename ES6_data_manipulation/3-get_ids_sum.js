ce883c1a1e3c16c3f4507d46e334ac7b1e1820a3
harmel3056 <melh1988@hotmail.com>
2026-01-06 19:06:58 +1100

    Uses reduce function to provide a count of array items

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,8 @@
+/* Returns the sum of student IDs in studentList array by using reduce(accumulator
+per student object within studentList. The student id must be therefore be accessed.
+(accumulator, student) are essentially the parameters for .reduce, and
+accumulator + student.id become the iterators with a 0 initialValue.
+*/
 export default function getStudentIdsSum(studentList) {
   return studentList.reduce((accumulator, student) => accumulator + student.id, 0);
 }
