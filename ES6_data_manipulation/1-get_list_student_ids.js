0f035c5b6b3d3c72564b40a220a837daeffea2c3
harmel3056 <melh1988@hotmail.com>
2026-01-07 14:24:02 +1100

    Updated with notes

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,6 @@
+/* Uses the map function with (student) of array of students
+in order to produce a list of corresponding student ids
+*/
 export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
