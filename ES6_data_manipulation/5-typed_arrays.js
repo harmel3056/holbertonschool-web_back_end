277ed68cf88c834d22f5e9da0d64a255d0a4c3f4
harmel3056 <melh1988@hotmail.com>
2026-01-08 14:00:59 +1100

    Returns and displays a new ArrayBuffer with an Int8 value at a specific position

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
 
