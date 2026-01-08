3406e8d3d59904fda04929bc8c8b4ca50bee63ec
harmel3056 <melh1988@hotmail.com>
2026-01-08 14:15:39 +1100

    Added notes

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error('Position outside range');
+    throw new Error('Position outside range'); // throws error where position will be outside given range
   }
 
-  const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length); //creates a buffer with the length parameter
+  const int8 = new Int8Array(buffer); //establishes 8-bit element view
 
-  int8[position] = value;
+  int8[position] = value; // fills new buffer with value entered at [position] in array
 
-  return new DataView(buffer)
+  return new DataView(buffer) // returns the newly filled buffer
 }
