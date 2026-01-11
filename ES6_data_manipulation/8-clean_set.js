ebaf2148fdd7f9650c64895e4e5b94e381f69a4c
harmel3056 <melh1988@hotmail.com>
2026-01-11 20:57:17 +1100

    Uses startsWith to identify objects in a set where characters match startString, and return the balance in a joined string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,19 @@
 export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) {
+  // Checks type is string to ensure startsWith doesn't throw an error if non-string is input
     return '';
   }
 
   const result = [];
 
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+  for (const object of set) { // Accessing items within the set
+    if (object.startsWith(startString)) { // Checks if string begins with chars of specified substring (bool)
+      result.push(object.slice(startString.length));
+      /* Here .slice takes a starting index based on the length of startString, and returns the
+      remainder of substring after that index point. 
+      push() appends the sliced string to the end of the result array. */
     }
   }
 
-  return result.join('-');
+  return result.join('-'); // takes the result array and joins it into a string
 }
