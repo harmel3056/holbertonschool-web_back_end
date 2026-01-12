c4b31178064347934434907090462b27ed9c454f
harmel3056 <melh1988@hotmail.com>
2026-01-13 07:54:24 +1100

    Demonstrates a Promise constructor

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+  /* Creates a new promise OBJECT, passes two functions (resolve, reject)
+  /  resolve -> call this when async work is successful
+  /  reject -> call this when async work fails */
+   });
+}
