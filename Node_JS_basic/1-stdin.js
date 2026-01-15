5036aaa199f07cf155bfbe0832ae2c6952551c8c
harmel3056 <melh1988@hotmail.com>
2026-01-15 15:27:05 +1100

    WIP for pc change

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+function processStdin() {
+    console.log{'Welcome to Holberton School, what is your name?'}
+    ...await
+    console.log{`Your name is: ${INPUT}'}
+    eventListener end program
+    console.log{'This important software is now closing'}
+}
