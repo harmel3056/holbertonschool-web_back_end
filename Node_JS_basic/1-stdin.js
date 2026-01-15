3a23cd91dd8454624f5c9730be6b220868a772fd
harmel3056 <melh1988@hotmail.com>
2026-01-15 16:56:11 +1100

    Uses the readline module to create a function that waits for an entry before returning a callback

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,17 @@
-function processStdin() {
-    console.log{'Welcome to Holberton School, what is your name?'}
-    ...await
-    console.log{`Your name is: ${INPUT}'}
-    eventListener end program
-    console.log{'This important software is now closing'}
-}
+const readline = require('readline'); // import the built-in readline module
+
+const rl = readline.createInterface({
+  // readline module allows the Node.js program to wait for user input
+  input: process.stdin, // necessary for readline
+  output: process.stdout, // optional for readline
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  // .question enacts a callback once an entry has been made and returned
+  // (name) becomes the parameter for that callback function
+  console.log(`Your name is: ${name}`);
+  // this is the callback, which uses the parameter (entry made by user)
+
+  console.log('This important software is now closing');
+  rl.close(); // function closes itself
+});
