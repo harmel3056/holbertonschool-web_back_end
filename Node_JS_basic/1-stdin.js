34d68be37f09f7e369d8b0c7e6f6e1b6ed8b1216
harmel3056 <melh1988@hotmail.com>
2026-01-15 17:08:12 +1100

    Checker compliance woes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+const { ChildProcess } = require('child_process');
 const readline = require('readline'); // import the built-in readline module
 
 const rl = readline.createInterface({
@@ -9,9 +10,9 @@ const rl = readline.createInterface({
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   // .question enacts a callback once an entry has been made and returned
   // (name) becomes the parameter for that callback function
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\r\n`);
   // this is the callback, which uses the parameter (entry made by user)
 
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
   rl.close(); // function closes itself
 });
