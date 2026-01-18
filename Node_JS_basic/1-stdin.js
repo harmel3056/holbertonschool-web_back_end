c0b45bc34a942a0f7737232998221819f475db95
harmel3056 <melh1988@hotmail.com>
2026-01-19 09:36:52 +1100

    Uses process.stdin eventlisteners to create responses to command-line entries

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-const readline = require('readline'); // import the built-in readline module
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  // readline module allows the Node.js program to wait for user input
-  input: process.stdin, // necessary for readline
-  output: process.stdout, // optional for readline
+let response = ''; /* variable buffer to accumulate command line entry by user.
+If we assigned the variable inside of process.stin.on it would override */
+
+process.stdin.on('data', (data) => { // when input arrives, run this function
+  response += data; // append stdin items to 'response' variable
+  process.stdout.write(`Your name is: ${response.trim()}\n`);
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // .question enacts a callback once an entry has been made and returned
-  // (name) becomes the parameter for that callback function
-  process.stdout.write(`Your name is: ${name}\r`);
-  // this is the callback, which uses the parameter (entry made by user)
-
-  process.on('exit', () => { // creates this process only if 'exit' enacted
-    process.stdout.write('This important software is now closing\n');
-  });
+process.on('exit', () => { // creates this process only if 'exit' enacted
+  process.stdout.write('This important software is now closing\n');
 });
