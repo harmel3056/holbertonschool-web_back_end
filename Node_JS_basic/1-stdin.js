431af0314ff0ffd8c2282c633269b76cd72e0624
harmel3056 <melh1988@hotmail.com>
2026-01-19 10:15:06 +1100

    Uses process.stdin eventlisteners to create responses to command-line entries

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let response = ''; /* variable buffer to accumulate command line entry by user.
-If we assigned the variable inside of process.stin.on it would override */
-
-process.stdin.on('data', (data) => { // when input arrives, run this function
-  response += data; // append stdin items to 'response' variable
-  process.stdout.write(`Your name is: ${response.trim()}\r`);
+process.stdin.on('data', (name) => { // when input arrives, run this function
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-process.on('exit', () => { // creates this process only if 'exit' enacted
+process.stdin.on('end', () => { // creates this process only when input ends
   process.stdout.write('This important software is now closing\n');
 });
