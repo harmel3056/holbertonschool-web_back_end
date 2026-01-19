3c258228710b6ac2d52fdda423a6345e3cfcf042
harmel3056 <melh1988@hotmail.com>
2026-01-19 18:52:46 +1100

    Adjusted to take third arg from command line instead of hardcoding database.csv - checker is running differently named file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,25 +3,26 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (request, response) => {
 // we make this async to accommodate async function countStudents
+  response.setHeader('Content-Type', 'text/plain');
+
   if (request.url === '/') {
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
+
     response.end('Hello Holberton School!');
     return;
   }
 
   if (request.url === '/students') {
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-
     response.write('This is the list of our students\n');
 
+    const database = process.argv[2];
+
     try {
-      const output = await countStudents('database.csv');
+      const output = await countStudents(database);
       // have to await async response or it may return before complete
       response.end(output);
     } catch (err) {
-      response.statusCode = 500;
       response.end('Cannot load the database');
     }
   }
