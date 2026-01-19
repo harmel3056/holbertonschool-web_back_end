a6be1fe4bd49bd13e75e99688a0c5879f445e853
harmel3056 <melh1988@hotmail.com>
2026-01-19 21:56:07 +1100

    Simple server using Express, pulling database data from 3-read_file

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.get('/students', async (request, response) => {
+  const database = process.argv[2];
+
+  try {
+    const output = await countStudents(database);
+    response.send(`This is the list of our students\n${output}`);
+  } catch (err) {
+    response.status(500).send('Cannot load the database');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
